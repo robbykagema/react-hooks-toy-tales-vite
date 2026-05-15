b2ec4e031962cdba82e3217a8ba9efa8a466bda5
robbykagema <133529729+robbykagema@users.noreply.github.com>
2026-05-15 09:55:08 +0300

    Update header image source URL

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <div id="toy-header">
       <img
-        src="https://fontmeme.com/permalink/180719/67429e6afec53d21d64643101c43f029.png"
+        src="https://fontmeme.com/permalink/180719/674291b52f115e516331273d31704e63.png"
         alt="toy header"
       />
     </div>
