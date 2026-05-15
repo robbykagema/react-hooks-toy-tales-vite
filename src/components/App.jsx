5f39eb4b734f5c7957117dcf83c6d5e05f9fad66
robbykagema <133529729+robbykagema@users.noreply.github.com>
2026-05-15 09:54:36 +0300

    Fetch toys and manage state in App component
    Added functionality to fetch toys from the server and manage toy state.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,50 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import ToyForm from "./ToyForm";
 import ToyContainer from "./ToyContainer";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [toys, setToys] = useState([]);
 
-  function handleClick() {
+  useEffect(() => {
+    fetch("http://localhost:3001/toys")
+      .then((r) => r.json())
+      .then((data) => setToys(data));
+  }, []);
+
+  function handleToggleForm() {
     setShowForm((showForm) => !showForm);
+  }
+
+  function handleAddToy(newToy) {
+    setToys([...toys, newToy]);
+  }
+
+  function handleDeleteToy(id) {
+    const updatedToys = toys.filter((toy) => toy.id !== id);
+    setToys(updatedToys);
+  }
+
+  function handleUpdateToy(updatedToy) {
+    const updatedToys = toys.map((toy) =>
+      toy.id === updatedToy.id ? updatedToy : toy
+    );
+    setToys(updatedToys);
   }
 
   return (
     <>
       <Header />
-      {showForm ? <ToyForm /> : null}
+      {showForm ? <ToyForm onAddToy={handleAddToy} /> : null}
       <div className="buttonContainer">
-        <button onClick={handleClick}>Add a Toy</button>
+        <button onClick={handleToggleForm}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer 
+        toys={toys} 
+        onDeleteToy={handleDeleteToy} 
+        onUpdateToy={handleUpdateToy} 
+      />
     </>
   );
 }
