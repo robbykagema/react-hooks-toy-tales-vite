83ef1ec011b126cc92929ebd7353ee9571a06ee6
robbykagema <133529729+robbykagema@users.noreply.github.com>
2026-05-15 09:55:48 +0300

    Refactor ToyCard to accept props and handle events

--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -1,17 +1,38 @@
 import React from "react";
 
-function ToyCard() {
+function ToyCard({ toy, onDeleteToy, onUpdateToy }) {
+  const { id, name, image, likes } = toy;
+
+  function handleLikeClick() {
+    const updatedLikes = likes + 1;
+
+    fetch(`http://localhost:3001/toys/${id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ likes: updatedLikes }),
+    })
+      .then((r) => r.json())
+      .then(onUpdateToy);
+  }
+
+  function handleDeleteClick() {
+    fetch(`http://localhost:3001/toys/${id}`, {
+      method: "DELETE",
+    })
+      .then(() => onDeleteToy(id));
+  }
+
   return (
     <div className="card" data-testid="toy-card">
-      <h2>{"" /* Toy's Name */}</h2>
-      <img
-        src={"" /* Toy's Image */}
-        alt={"" /* Toy's Name */}
-        className="toy-avatar"
-      />
-      <p>{"" /* Toy's Likes */} Likes </p>
-      <button className="like-btn">Like {"<3"}</button>
-      <button className="del-btn">Donate to GoodWill</button>
+      <h2>{name}</h2>
+      <img src={image} alt={name} className="toy-avatar" />
+      <p>{likes} Likes </p>
+      <button className="like-btn" onClick={handleLikeClick}>
+        Like {"<3"}
+      </button>
+      <button className="del-btn" onClick={handleDeleteClick}>
+        Donate to GoodWill
+      </button>
     </div>
   );
 }
