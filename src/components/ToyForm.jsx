42f37e462c9ff4e75c570d11e60c485c6ed3d891
robbykagema <133529729+robbykagema@users.noreply.github.com>
2026-05-15 10:00:07 +0300

    Implement ToyForm for adding new toys
    Refactor ToyContainer to ToyForm and add toy creation functionality.

--- src/components/ToyForm.jsx
+++ src/components/ToyForm.jsx
@@ -1,19 +1,61 @@
-import React from "react";
-import ToyCard from "./ToyCard";
+import React, { useState } from "react";
 
-function ToyContainer({ toys, onDeleteToy, onUpdateToy }) {
+function ToyForm({ onAddToy }) {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const newToy = {
+      name,
+      image,
+      likes: 0,
+    };
+
+    fetch("http://localhost:3001/toys", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newToy),
+    })
+      .then((r) => r.json())
+      .then((data) => {
+        onAddToy(data);
+        setName("");
+        setImage("");
+      });
+  }
+
   return (
-    <div id="toy-collection">
-      {toys.map((toy) => (
-        <ToyCard 
-          key={toy.id} 
-          toy={toy} 
-          onDeleteToy={onDeleteToy} 
-          onUpdateToy={onUpdateToy} 
+    <div className="container">
+      <form onSubmit={handleSubmit} className="add-toy-form">
+        <h3>Create a toy!</h3>
+        <input
+          type="text"
+          name="name"
+          placeholder="Enter a toy's name..."
+          className="input-text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
         />
-      ))}
+        <br />
+        <input
+          type="text"
+          name="image"
+          placeholder="Enter a toy's image URL..."
+          className="input-text"
+          value={image}
+          onChange={(e) => setImage(e.target.value)}
+        />
+        <br />
+        <input
+          type="submit"
+          name="submit"
+          value="Create New Toy"
+          className="submit"
+        />
+      </form>
     </div>
   );
 }
 
-export default ToyContainer;
+export default ToyForm;
